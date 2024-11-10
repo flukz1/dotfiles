212463e3b0759c4cc716b15e70362a061c908428
Xani <q787615@gmail.com>
2024-11-10 16:30:01 +0500

    Update change-profile.sh

--- config/kitty/change-profile.sh
+++ config/kitty/change-profile.sh
@@ -8,11 +8,19 @@ read -r profile
 if [ "$profile" -eq 1 ]; then
   rm ~/.config/kitty/kitty.conf
   cp ~/.config/kitty/kitty-coding.conf ~/.config/kitty/kitty.conf
+  
+  rm ~/.zshrc
+  cp ~/.zshrc-coding ~/.zshrc
   echo "Get in the flow!"
+
 elif [ "$profile" -eq 2 ]; then
   rm ~/.config/kitty/kitty.conf
   cp ~/.config/kitty/kitty-default.conf ~/.config/kitty/kitty.conf
+  
+  rm ~/.zshrc
+  cp ~/.zshrc-default ~/.zshrc
   echo "Have fun!"
+
 else
   echo "What the hell?"
 fi
