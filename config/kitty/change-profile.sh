2de92f8e1caff739196d59116ed73cece8a914bd
Xani <q787615@gmail.com>
2024-11-10 14:01:26 +0500

    Add files via upload

---
+++ config/kitty/change-profile.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Select a profile:"
+echo "   (1) Coding"
+echo "   (2) Default"
+read -r profile
+
+if [ "$profile" -eq 1 ]; then
+  rm ~/.config/kitty/kitty.conf
+  cp ~/.config/kitty/kitty-coding.conf ~/.config/kitty/kitty.conf
+  echo "Get in the flow!"
+elif [ "$profile" -eq 2 ]; then
+  rm ~/.config/kitty/kitty.conf
+  cp ~/.config/kitty/kitty-default.conf ~/.config/kitty/kitty.conf
+  echo "Have fun!"
+else
+  echo "What the hell?"
+fi
